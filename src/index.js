f30039546bf75c59a42c72ec90363438ee7a5bd7
Johannes Olsson <johannes.olsson.9779@student.uu.se>
2022-10-26 21:42:23 +0200

    Found new guide

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
 );
 
