11a83907f04fd992688698dbbf060020b2fce56f
Johonka <95031051+Johonka@users.noreply.github.com>
2023-12-30 21:39:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import DataComponent from './DataComponent';
+import AddProductForm from './AddProductForm';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App med Server</h1>
+      <DataComponent />
+      <div className="AdminGUI">
+        <h2>Tillfälligt Admin GUI</h2>
+      </div>
+      <AddProductForm />
     </div>
   );
 }
